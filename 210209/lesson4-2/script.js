790d1334195d8e101522760303cc2ed4f9282746
minjeongkang <snowdrop1202@gmail.com>
2021-02-11 00:01:28 +0900

    refactor: 4-2 refactoring

--- 210209/lesson4-2/script.js
+++ 210209/lesson4-2/script.js
@@ -1,20 +1,30 @@
 const solution = (x, a) => {
   const arr = [];
   let max = 0;
+  let limitMax = 0;
 
-  for (let i = 0; i < x; i ++) arr[i] = 0;
+  for (let i = 0; i < x; i ++) {
+    arr[i] = 0
+  };
 
   a.map((r) => {
     const num = r - 1;
-    if (r < x) {
+    if (r <= x) {
+      arr[num] = Math.max.apply(null, [limitMax, arr[num]]);
       arr[num] += 1;
+      max = Math.max.apply(null, [max, arr[num]]);
     } else {
-      max = Math.max.apply(null, arr);
-      arr.fill(max);
+      limitMax = max;
     }
   });
+
+  arr.map((a, i) => {
+    arr[i] = Math.max.apply(null, [a, limitMax])
+  });
+
   return arr;
 };
+
 solution(5, [3, 4, 4, 6, 1, 4, 4]);
 
 solution(5, [6, 6, 6, 6, 6, 6, 6, 6]);
