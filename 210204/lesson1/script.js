24908bc5c330a80aa8f3e17cc54621200cd3a4c5
minjeongkang <snowdrop1202@gmail.com>
2021-02-11 22:13:20 +0900

    refactor: lesson1 refactoring

--- 210204/lesson1/script.js
+++ 210204/lesson1/script.js
@@ -1,13 +1,18 @@
-const solution = (decimal) => {
-  if (decimal > 1000000) return;
-  const binary = decimal.toString(2).replace(/1/gi, "/");
+const solution = (a) => {
+  const str = a.toString(2).replace(/1/gi, "_");
 
-  if (binary.indexOf("/") === binary.lastIndexOf("/")) return 0;
+  if (str.indexOf("0") < 0) return 0;
+  if (str.indexOf("_") === str.lastIndexOf("_")) return 0;
 
-  const binaryArray = binary.split("/");
-  const binaryIter = binaryArray.map((a) => a.length);
+  const arr = str.toString().split("_");
+  if (str[str.length - 1] === "0") arr.pop();
+  const length = arr.length;
+  let max = 0;
+  for (let i = 0; i < length; i++) {
+    if (max < arr[i].length) {
+      max = arr[i].length;
+    }
+  }
 
-  return Number(Math.max(...binaryIter));
+  return max;
 };
-
-solution(1232323);
