cbbd1e6e635606974aef9c380c57546adc89904e
minjeongkang <snowdrop1202@gmail.com>
2021-02-14 01:04:32 +0900

    refactor: lesson 5-2 refactoring

--- 210213/lesson5-2/script.js
+++ 210213/lesson5-2/script.js
@@ -1,16 +1,16 @@
 const solution = (s, p, q) => {
-  const obj = {
-    A: 1,
-    C: 2,
-    G: 3,
-    T: 4
-  };
-  const str = s.toUpperCase().split("");
-  const arr = p.map((a, i) => {
-    return Math.min(obj[str[a]], obj[str[q[i]]]);
-  });
-  console.log(arr);
+  let arr = [];
+
+  for (let i = 0; i < p.length; i++) {
+    let str = s.slice(p[i], q[i] + 1);
+    if (str.indexOf("A") > -1) arr.push(1);
+    else if (str.indexOf("C") > -1) arr.push(2);
+    else if (str.indexOf("G") > -1) arr.push(3);
+    else arr.push(4);
+  }
+
   return arr;
 };
 
 solution("CAGCCTA", [2, 5, 0], [4, 5, 6]);
+solution("TC", [0, 0, 1], [0, 1, 1]);
